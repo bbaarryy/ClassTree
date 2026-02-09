e63acb92bc6de6548312d532bb14f7088c7d18a0
DimonLenovo <you@example.com>
2026-02-09 17:53:51 +0300

    start_class

--- main.cpp
+++ main.cpp
@@ -3,7 +3,19 @@
 
 using namespace::std;
 
+class Plant{
+    protected:
+        float max_height;
+        string climate;
 
+    public:
+        virtual int Danger() = 0;
+        virtual int LifePossibility() = 0;
+};
+
+class TropicalPlants: public Plant{
+    
+};
 
 int main(){
     //
